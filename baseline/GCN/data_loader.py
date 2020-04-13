999b47b8ecbfe026ecc4484071d2c89ed85eb70e
xcwujie123 <32511949+xcwujie123@users.noreply.github.com>
2020-04-14 00:50:40 +0800

    Add files via upload

---
+++ baseline/GCN/data_loader.py
@@ -0,0 +1,109 @@
+import numpy as np
+import os
+
+
+def load_data(args):
+    n_user, n_item, train_data, eval_data, test_data = load_rating(args)
+    n_entity, n_relation, adj_entity, adj_relation = load_kg(args)
+    print('data loaded.')
+
+    return n_user, n_item, n_entity, n_relation, train_data, eval_data, test_data, adj_entity, adj_relation
+
+def load_rating(args):
+    rating_file = 'KGCN/interaction'
+    if os.path.exists(rating_file + '.npy'):
+        rating_np = np.load(rating_file + '.npy')
+    else:
+        rating_np = np.loadtxt(rating_file + '.txt', dtype=np.int64)
+        np.save(rating_file + '.npy', rating_np)
+    n_user = 6166
+    n_item = 633
+    train_data, eval_data, test_data = dataset_split(rating_np, args)
+
+    return n_user, n_item, train_data, eval_data, test_data
+
+def dataset_split(rating_np, args):
+    print('splitting dataset ...')
+
+    # train:eval:test = 6:2:2
+    eval_ratio = 0.2
+    test_ratio = 0.2
+    n_ratings = rating_np.shape[0]
+
+    eval_indices = np.random.choice(list(range(n_ratings)), size=int(n_ratings * eval_ratio), replace=False)
+    left = set(range(n_ratings)) - set(eval_indices)
+    test_indices = np.random.choice(list(left), size=int(n_ratings * test_ratio), replace=False)
+    train_indices = list(left - set(test_indices))
+    if args.ratio < 1:
+        train_indices = np.random.choice(list(train_indices), size=int(len(train_indices) * args.ratio), replace=False)
+
+    train_data = rating_np[train_indices]
+    eval_data = rating_np[eval_indices]
+    test_data = rating_np[test_indices]
+
+    return train_data, eval_data, test_data
+
+
+
+def load_kg(args):
+    print('reading KG file ...')
+
+    # reading kg file
+    kg_file = 'KGCN/kg_new'
+    if os.path.exists(kg_file + '.npy'):
+        kg_np = np.load(kg_file + '.npy')
+    else:
+        kg_np = np.loadtxt(kg_file + '.txt', dtype=np.int64)
+        np.save(kg_file + '.npy', kg_np)
+
+    n_entity = 755
+    n_relation = len(set(kg_np[:, 1]))
+
+    kg = construct_kg(kg_np)
+    adj_entity, adj_relation = construct_adj(args, kg, n_entity)
+
+    return n_entity, n_relation, adj_entity, adj_relation
+
+
+def construct_kg(kg_np):
+    print('constructing knowledge graph ...')
+    kg = dict()
+    for triple in kg_np:
+        head = triple[0]
+        relation = triple[1]
+        tail = triple[2]
+        # treat the KG as an undirected graph
+        if head not in kg:
+            kg[head] = []
+        kg[head].append((tail, relation))
+        if tail not in kg:
+            kg[tail] = []
+        kg[tail].append((head, relation))
+    return kg
+
+
+def construct_adj(args, kg, entity_num):
+    print('constructing adjacency matrix ...')
+    # each line of adj_entity stores the sampled neighbor entities for a given entity
+    # each line of adj_relation stores the corresponding sampled neighbor relations
+    arr_weight=np.load("weight.npy")
+    adj_entity = np.zeros([entity_num, args.neighbor_sample_size], dtype=np.int64)
+    adj_relation = np.zeros([entity_num, args.neighbor_sample_size], dtype=np.int64)
+    list_del=[]
+    for i in range(arr_weight.shape[0]):
+        s=np.sum(arr_weight[i,:])
+        if s==0:
+            list_del.append(i)
+    for entity in range(entity_num):
+        if entity in list_del:
+            continue
+        neighbors = kg[entity]
+        n_neighbors = len(neighbors)
+        if n_neighbors >= args.neighbor_sample_size:
+            sampled_indices = np.random.choice(list(range(n_neighbors)), size=args.neighbor_sample_size, replace=False)
+        else:
+            sampled_indices = np.random.choice(list(range(n_neighbors)), size=args.neighbor_sample_size, replace=True)
+        adj_entity[entity] = np.array([neighbors[i][0] for i in sampled_indices])
+        adj_relation[entity] = np.array([neighbors[i][1] for i in sampled_indices])
+
+    return adj_entity, adj_relation
